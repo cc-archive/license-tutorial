ada0358120c78b17f4b64c5c994102bb65f75d09
bse <bse@committer.creativecommons.org>
2007-05-02 00:08:04 +0000

    jurisdiction title; also center h2's...

--- 8.php
+++ 8.php
@@ -5,6 +5,8 @@ $page_content = '
 <div class="block">
 <p><img src="images/jurisdiction.png" /></p>
 <p>You can select the jurisdiction of the license that will govern your work.</p>
+
+<h2>Must I select a jurisdiction?</h2>
 <p>You should select a jurisdiction if you want a license that is governed by the copyright law of your specific jurisdiction. However, you\'re not required to select a jurisdiction.</p>
 </div>
 ';
